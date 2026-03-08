8ec4a8b2ae31ad741d12b9cd69dfda9f3fed857b
AkobirCoder <akobir0308@gmail.com>
2026-03-09 00:21:22 +0500

    _component/social.tsx was added to client/app/auth/_component/social.tsx

---
+++ client/app/auth/_component/social.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button } from '@/components/ui/button';
+import { FaGithub, FaGoogle } from 'react-icons/fa';
+
+const Social = () => {
+    return (
+        <div className='grid grid-cols-2 w-full gap-1'>
+            <Button variant={'outline'}>
+                <span>Sign up with Google</span>
+                <FaGoogle />
+            </Button>
+            <Button variant={'secondary'}>
+                <span>Sign up with GitHub</span>
+                <FaGithub />
+            </Button>
+        </div>
+    );
+}
+
+export default Social;
