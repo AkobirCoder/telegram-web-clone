85cfad16e6b0d5571c902adb62fe80676e7112ac
AkobirCoder <akobir0308@gmail.com>
2026-03-09 00:25:23 +0500

    client/lib/validation.ts was added

---
+++ client/lib/validation.ts
@@ -0,0 +1,11 @@
+import * as z from "zod";
+ 
+export const emailSchema = z.object({
+    email: z.string().email({message: 'Invalid email address, please check try and again.'}),
+});
+
+export const otpSchema = z.object({
+    otp: z.string().min(6, {
+        message: 'Your one-time password must be 6 characters.',
+    })
+}).merge(emailSchema);
