213ef7b86223cc404e74775d800928b393fbe57d
AkobirCoder <akobir0308@gmail.com>
2026-03-09 20:49:55 +0500

    use-current.ts was added to client/hooks/use-current.ts

---
+++ client/hooks/use-current.ts
@@ -0,0 +1,14 @@
+import { IUser } from '@/types';
+import { useState } from 'react';
+
+const useCurrentContact = () => {
+    const [currentContact, setCurrentContact] = useState<IUser | null>(null);
+
+    return (
+        {
+            currentContact, setCurrentContact,
+        }
+    );
+}
+
+export default useCurrentContact;
