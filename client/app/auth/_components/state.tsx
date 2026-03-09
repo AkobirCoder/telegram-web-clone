037848e8df7139d65ba5683581ae11497d513791
AkobirCoder <akobir0308@gmail.com>
2026-03-09 20:48:19 +0500

    state.tsx was moved to client/app/auth/_components/state.tsx

---
+++ client/app/auth/_components/state.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import React from 'react';
+import SignIn from './sign-in';
+import Verify from './verify';
+import { useAuth } from '@/hooks/use-auth';
+
+const StateAuth = () => {
+    const {step} = useAuth();
+
+    return (
+        <>
+            {step === 'login' && <SignIn />}
+            {step === 'verify' && <Verify />}
+        </>
+    );
+}
+
+export default StateAuth;
