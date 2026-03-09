4bb40d927b72301037d1ac1735140dd8cce09285
AkobirCoder <akobir0308@gmail.com>
2026-03-09 17:51:53 +0500

    client/app/auth/page.tsx was updated

--- client/app/auth/page.tsx
+++ client/app/auth/page.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { FaTelegram } from 'react-icons/fa';
 import StateAuth from './_component/state';
 import Social from './_component/social';
+import { ModeToggle } from '@/components/shared/mode-toggle';
 
 const Page = () => {
     return (
         <div className='m-auto max-w-md w-full h-screen flex justify-center items-center flex-col space-y-4'>
             <FaTelegram size={120} className='text-blue-500' />
-            <div className=''>
+            <div className='flex items-center gap-2'>
                 <h1 className='text-4xl font-bold'>Telegram</h1>
+                <ModeToggle />
             </div>
             <StateAuth />
             <Social />
