e67e0618b280d070052321897b00a16532e0a7e2
AkobirCoder <akobir0308@gmail.com>
2026-03-09 17:50:36 +0500

    client/components/shared/mode-toggle.tsx was added

---
+++ client/components/shared/mode-toggle.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import * as React from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+
+export function ModeToggle() {
+    const { setTheme, resolvedTheme } = useTheme();
+
+    // const [mounted, setMounted] = React.useState(false);
+
+    // React.useEffect(() => {
+    //     setMounted(true);
+    // }, []);
+
+    // if (!mounted) return null;
+
+    return (
+        <>
+            {
+                resolvedTheme === 'dark' ? (
+                    <Button size={'icon'} variant={'ghost'} onClick={() => setTheme('light')}>
+                        <Sun />
+                    </Button>
+                ): (
+                    <Button size={'icon'} variant={'ghost'} onClick={() => setTheme('dark')}>
+                        <Moon />
+                    </Button>
+                )
+            }
+        </>
+    );
+}
