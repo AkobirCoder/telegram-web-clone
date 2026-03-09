53094ffc0ed2e4ec22281951a209dd0b0a470ad7
AkobirCoder <akobir0308@gmail.com>
2026-03-09 20:43:04 +0500

    client/app/(chat)/_components/settings.tsx was added

---
+++ client/app/(chat)/_components/settings.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button';
+import { Menu } from 'lucide-react';
+import React from 'react';
+
+const Settings = () => {
+    return (
+        <>
+            <Button size={'icon'} variant={'secondary'}>
+                <Menu />
+            </Button>
+        </>
+    );
+}
+
+export default Settings;
