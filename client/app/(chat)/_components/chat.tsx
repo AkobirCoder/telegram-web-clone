373f84e929d27b7030765f519d7652cbd353a9ac
AkobirCoder <akobir0308@gmail.com>
2026-03-11 00:36:58 +0500

    client/app/(chat)/_components/chat.tsx was updated

--- client/app/(chat)/_components/chat.tsx
+++ client/app/(chat)/_components/chat.tsx
@@ -5,9 +5,13 @@ import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { messageSchema } from '@/lib/validation';
 import { Paperclip, Send, Smile } from 'lucide-react';
-import React, { FC } from 'react';
+import React, { FC, useRef } from 'react';
 import { UseFormReturn } from 'react-hook-form';
 import z from 'zod';
+import emojies from '@emoji-mart/data';
+import Picker from '@emoji-mart/react';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { useTheme } from 'next-themes';
 
 interface Props {
     messageForm: UseFormReturn<z.infer<typeof messageSchema>>,
@@ -15,8 +19,34 @@ interface Props {
 }
 
 const Chat: FC<Props> = ({messageForm, onSendMessage}) => {
+    const {resolvedTheme} = useTheme();
+
+    const inputRef = useRef<HTMLInputElement | null>(null);
+
+    const handleEmojiSelect = (emoji: string) => {
+        const input = inputRef.current;
+
+        if (!input) return;
+
+        const text = messageForm.getValues('text');
+
+        const start = input.selectionStart ?? 0;
+
+        const end = input.selectionEnd ?? 0;
+
+        const newText = text.slice(0, start) + emoji + text.slice(end);
+
+        messageForm.setValue('text', newText);
+
+        setTimeout(() => {
+            input.setSelectionRange(start + emoji.length, start + emoji.length);
+        }, 0);
+
+        // messageForm.setValue('text', messageForm.getValues('text') + emoji);
+    }
+    
     return (
-        <div className='flex flex-col justify-end z-40 h-[92vh]'>
+        <div className='flex flex-col justify-end z-40 min-h-[92vh]'>
             <div className='flex-1 overflow-y-auto'>
                 {/* --- Loading --- */}
                 <ChatLoading />
@@ -55,9 +85,19 @@ const Chat: FC<Props> = ({messageForm, onSendMessage}) => {
             <Form {...messageForm}>
                 <form 
                     onSubmit={messageForm.handleSubmit(onSendMessage)} 
-                    className='flex items-center w-full p-2 sticky z-30 bottom-0 bg-background/90 backdrop-blur-md'
+                    className={`
+                        flex items-center w-full p-2 
+                        sticky z-30 bottom-0 
+                        bg-background/50 backdrop-blur-sm
+                        border-t border-t-zinc-300 dark:border-t-0
+                    `}
                 >
-                    <Button size={'icon'} className='h-9 border-0' type='button' variant={'secondary'}>
+                    <Button 
+                        size={'icon'} 
+                        className='h-9 border-r-0 border-zinc-300 dark:border-0' 
+                        type='button' 
+                        variant={'secondary'}
+                    >
                         <Paperclip />
                     </Button>
                     <FormField
@@ -77,14 +117,34 @@ const Chat: FC<Props> = ({messageForm, onSendMessage}) => {
                                         value={field.value}
                                         onChange={(e) => field.onChange(e.target.value)}
                                         onBlur={() => field.onBlur()}
+                                        ref={inputRef}
                                     />
                                 </FormControl>
                             </FormItem>
                         )}
                     />
-                    <Button size={'icon'} className='h-9 border-0' type='button' variant={'secondary'}>
-                        <Smile />
-                    </Button>
+                    <Popover>
+                        <PopoverTrigger asChild>
+                            <Button 
+                                size={'icon'} 
+                                className='h-9 border-l-0 border-zinc-300 dark:border-0' 
+                                type='button' 
+                                variant={'secondary'}
+                            >
+                                <Smile />
+                            </Button>
+                        </PopoverTrigger>
+                        <PopoverContent 
+                            className='p-0 border-none rounded-md absolute right-4 bottom-14'
+                        >
+                            <Picker 
+                                data={emojies} 
+                                theme={resolvedTheme === 'dark' ? 'dark' : 'light'} 
+                                onEmojiSelect={(emoji: {native: string}) => handleEmojiSelect(emoji.native)}
+                            />
+                        </PopoverContent>
+                    </Popover>
+                    
                     <Button type='submit' className='h-9 border-0' size={'icon'}>
                         <Send />
                     </Button>
