4e8545a58366552ef77f908d7934030f00059434
AkobirCoder <akobir0308@gmail.com>
2026-03-11 09:56:43 +0500

    client/components/loadings/message.loading.tsx was updated

--- client/components/loadings/message.loading.tsx
+++ client/components/loadings/message.loading.tsx
@@ -8,8 +8,15 @@ interface Props {
 
 const MessageLoading: FC<Props> = ({isReceived}) => {
     return (
-        <div className={cn('m-2.5 font-medium text-xs flex', isReceived ? 'justify-start' : 'justify-end')}>
-            <Skeleton className={cn('relative inline p-2.5 pr-12', isReceived ? 'bg-primary/20' : 'bg-secondary-foreground/20')}>
+        <div className={cn('m-2.5 font-medium text-xs flex', isReceived 
+            ? 'justify-start' 
+            : 'justify-end'
+        )}>
+            <Skeleton className={cn('relative inline p-2.5 pr-12', isReceived 
+                ? 'bg-primary/20' 
+                : 'bg-secondary-foreground/20'
+            )}>
+                <Skeleton className='w-36 h-5 mb-2' />
                 <Skeleton className='w-36 h-5' />
                 <span className='absolute text-xs right-1 bottom-0 opacity-60'>✓</span>
             </Skeleton>
