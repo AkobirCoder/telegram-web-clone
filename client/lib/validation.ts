0347b05a1c090606d91e93c607c9089ca9e6c5ce
AkobirCoder <akobir0308@gmail.com>
2026-03-11 09:59:57 +0500

    client/lib/validation.ts was updated

--- client/lib/validation.ts
+++ client/lib/validation.ts
@@ -7,10 +7,16 @@ export const emailSchema = z.object({
 export const otpSchema = z.object({
     otp: z.string().min(6, {
         message: 'Your one-time password must be 6 characters.',
-    })
+    }),
 }).merge(emailSchema);
 
 export const messageSchema = z.object({
     text: z.string().min(1, {message: 'Message cannot be empty.'}),
     image: z.string().optional(),
+});
+
+export const profileSchema = z.object({
+    firstName: z.string().min(2),
+    lastName: z.string().optional(),
+    bio: z.string().optional(),
 });
