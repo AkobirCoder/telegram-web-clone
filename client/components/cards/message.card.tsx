15b342bf168940584f595e31dae0eb13625b04c8
AkobirCoder <akobir0308@gmail.com>
2026-03-11 09:53:57 +0500

    client/components/cards/message.card.tsx was updated

--- client/components/cards/message.card.tsx
+++ client/components/cards/message.card.tsx
@@ -7,7 +7,10 @@ interface Props {
 
 const MessageCard: FC<Props> = ({isReceived}) => {
     return (
-        <div className={cn('m-2.5 font-medium text-xs flex', isReceived ? 'justify-start' : 'justify-end')}>
+        <div className={cn('m-2.5 font-medium text-xs flex', isReceived 
+            ? 'justify-start' 
+            : 'justify-end'
+        )}>
             <div className={cn('relative inline p-2.5 pr-12 max-w-full', isReceived 
                 ? 'bg-primary' 
                 : 'bg-secondary-foreground/20 border border-zinc-400 dark:border-0'
