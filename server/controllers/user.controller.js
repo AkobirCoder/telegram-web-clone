7162298cfe3586bae5b1bf1f6d0c674f206b6a3c
AkobirCoder <akobir0308@gmail.com>
2026-03-15 21:31:59 +0500

    user.controller.js was added to server/controllers/

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,52 @@
+const { CONST } = require('../lib/constants');
+const messageModel = require('../models/message.model');
+
+class UserController {
+    // [GET]
+    async getMessages(req, res, next) {
+        try {
+            const user = '69b5c6fdc5b403a211e7c184';
+
+            const {contactId} = req.params;
+
+            const messages = await messageModel
+                .find({
+                    $or: [
+                        {sender: user, receiver: contactId},
+                        {sender: contactId, receiver: user},
+                    ],
+                })
+                .populate({path: 'sender', select: 'email'})
+                .populate({path: 'receiver', select: 'email'});
+
+            await messageModel.updateMany(
+                {sender: contactId, receiver: user, status: 'SENT'},
+                {status: CONST.READ}
+            );  
+
+            res.status(200).json({messages});
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    // [POST]
+    async createMessage(req, res, next) {
+        try {
+            const {sender, receiver, ...payload} = req.body;
+
+            const newMessage = await messageModel.create(req.body);
+
+            const currentMessage = await messageModel
+                .findById(newMessage._id)
+                .populate({path: 'sender', select: 'email'})
+                .populate({path: 'receiver', select: 'email'});
+
+            res.status(201).json({newMessage: currentMessage});
+        } catch (error) {
+            next(error);
+        }
+    }
+}
+
+module.exports = new UserController();
