2d030d8420275470b3122111313d2941d57605c6
AkobirCoder <akobir0308@gmail.com>
2026-03-20 22:10:58 +0500

    client/lib/utils.ts was updated

--- client/lib/utils.ts
+++ client/lib/utils.ts
@@ -2,5 +2,24 @@ import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs))
+}
+
+export const getSoundLabel = (value?: string) => {
+    switch (value) {
+        case 'notification.mp3':
+            return 'Apple';
+
+        case 'notification2.mp3':
+            return 'Sammish';
+
+        case 'sending.mp3':
+            return 'Belli';
+
+        case 'sending2.mp3':
+            return 'Oranger';
+
+        default:
+            return '';
+    }
 }
