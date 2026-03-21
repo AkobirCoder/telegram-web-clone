be9330bafbb674b383de8cedf32c4023b3d6b270
AkobirCoder <akobir0308@gmail.com>
2026-03-22 00:48:26 +0500

    client/components/ui/sheet.tsx was updated

--- client/components/ui/sheet.tsx
+++ client/components/ui/sheet.tsx
@@ -72,7 +72,7 @@ function SheetContent({
           <SheetPrimitive.Close data-slot="sheet-close" asChild>
             <Button
               variant="ghost"
-              className="absolute rounded-none top-3 right-3"
+              className="absolute rounded-none top-3 right-3 border-none"
               size="icon-sm"
             >
               <XIcon
