76a137f540eb55439f16291305957e016be47e90
AkobirCoder <akobir0308@gmail.com>
2026-03-21 18:27:39 +0500

    client/app/(chat)/_components/add-contact.tsx was updated

--- client/app/(chat)/_components/add-contact.tsx
+++ client/app/(chat)/_components/add-contact.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/ui/button';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { useLoading } from '@/hooks/use-loading';
 import { emailSchema } from '@/lib/validation';
 import React, { FC } from 'react';
 import { UseFormReturn } from 'react-hook-form';
@@ -13,6 +14,8 @@ interface Props {
 }
 
 const AddContact: FC<Props> = ({contactForm, onCreateContact}) => {
+    const {isCreating} = useLoading();
+
     return (
         <div className='h-screen w-full flex z-40 relative'>
             <div className='flex justify-center items-center z-50 w-full'>
@@ -34,13 +37,21 @@ const AddContact: FC<Props> = ({contactForm, onCreateContact}) => {
                                                 placeholder='example@gmail.com' 
                                                 className='h-10 bg-secondary' 
                                                 {...field} 
+                                                disabled={isCreating}
                                             />
                                         </FormControl>
                                         <FormMessage className='text-xs text-red-500 mt-1' />
                                     </FormItem>
                                 )}
                             />   
-                            <Button type='submit' className='w-full' size={'lg'}>Submit</Button>
+                            <Button 
+                                type='submit' 
+                                className='w-full' 
+                                size={'lg'}
+                                disabled={isCreating}
+                            >
+                                Submit
+                            </Button>
                         </form>
                     </Form>
                 </div>
