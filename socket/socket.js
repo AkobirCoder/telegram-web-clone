e0ad98ead8fb93a21816e9d8176f9fb1b34decd7
AkobirCoder <akobir0308@gmail.com>
2026-03-23 00:55:55 +0500

    socket/socket.js was updated

--- socket/socket.js
+++ socket/socket.js
@@ -17,6 +17,12 @@ const addOnlineUser = (user, socketId) => {
     }
 }
 
+const getSocketId = (userId) => {
+    const user = users.find((u) => u.user._id === userId);
+
+    return user ? user.socketId : null;
+}
+
 io.on('connection', (socket) => {
     console.log('User connected', socket.id);
 
@@ -26,6 +32,16 @@ io.on('connection', (socket) => {
         addOnlineUser(user, socket.id);
 
         io.emit('getOnlineUsers', users);
+    });
+
+    socket.on('createContact', ({currentUser, receiver}) => {
+        const receiverSocketId = getSocketId(receiver._id);
+
+        console.log('receiverSocketId', receiverSocketId);
+
+        if (receiverSocketId) {
+            socket.to(receiverSocketId).emit('getCreatedUser', currentUser);
+        }
     });
 
     socket.on('disconnect', () => {
