e1615f330e7e1d42ca491bb7514d16c0012ec485
AkobirCoder <akobir0308@gmail.com>
2026-03-23 23:05:20 +0500

    socket/socket.js was updated

--- socket/socket.js
+++ socket/socket.js
@@ -37,10 +37,18 @@ io.on('connection', (socket) => {
     socket.on('createContact', ({currentUser, receiver}) => {
         const receiverSocketId = getSocketId(receiver._id);
 
-        console.log('receiverSocketId', receiverSocketId);
+        // console.log('receiverSocketId', receiverSocketId);
 
         if (receiverSocketId) {
             socket.to(receiverSocketId).emit('getCreatedUser', currentUser);
+        }
+    });
+
+    socket.on('sendMessage', ({newMessage, receiver, sender}) => {
+        const receiverSocketId = getSocketId(receiver._id);
+
+        if (receiverSocketId) {
+            socket.to(receiverSocketId).emit('getNewMessage', {newMessage, receiver, sender});
         }
     });
 
