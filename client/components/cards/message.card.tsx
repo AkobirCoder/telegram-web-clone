85b592e5c3211178ba3b550615d03e4f31f36a17
AkobirCoder <akobir0308@gmail.com>
2026-03-23 23:04:20 +0500

    client/components/cards/message.card.tsx was updated

--- client/components/cards/message.card.tsx
+++ client/components/cards/message.card.tsx
@@ -1,21 +1,34 @@
+import { useCurrentContact } from '@/hooks/use-current';
 import { cn } from '@/lib/utils';
+import { IMessage } from '@/types';
 import React, { FC } from 'react';
 
 interface Props {
-    isReceived?: boolean,
+    // isReceived?: boolean,
+    message: IMessage,
 }
 
-const MessageCard: FC<Props> = ({isReceived}) => {
+const MessageCard: FC<Props> = ({message}) => {
+    const {currentContact} = useCurrentContact();
+
     return (
-        <div className={cn('m-2.5 font-medium text-xs flex', isReceived 
+        <div className={cn('m-2.5 font-medium text-xs flex', 
+            message.receiver._id !== currentContact?._id 
             ? 'justify-start' 
             : 'justify-end'
         )}>
-            <div className={cn('relative inline p-2.5 pr-12 max-w-full', isReceived 
+            <div className={cn('relative inline p-2.5 pr-12 max-w-full', 
+                message.receiver._id === currentContact?._id 
                 ? 'bg-primary' 
                 : 'bg-secondary-foreground/20 border border-zinc-400 dark:border-0'
             )}>
-                <p className={cn('text-sm', isReceived ? 'text-white' : '')}>Hello world</p>
+                <p className={cn('text-sm', 
+                    message.receiver._id !== currentContact?._id 
+                    ? 'text-white' 
+                    : ''
+                )}>
+                    {message.text}
+                </p>
                 <span className='absolute text-xs right-1 bottom-0 opacity-60'>✓</span>
             </div>
         </div>
