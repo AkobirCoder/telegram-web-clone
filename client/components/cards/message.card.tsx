969cb96fdc75a8ae0587a11c0e419d2c34e1661b
AkobirCoder <akobir0308@gmail.com>
2026-03-25 23:54:07 +0500

    client/components/cards/message.card.tsx was updated

--- client/components/cards/message.card.tsx
+++ client/components/cards/message.card.tsx
@@ -1,6 +1,9 @@
 import { useCurrentContact } from '@/hooks/use-current';
+import { CONST } from '@/lib/constants';
 import { cn } from '@/lib/utils';
 import { IMessage } from '@/types';
+import { format } from 'date-fns';
+import { Check, CheckCheck } from 'lucide-react';
 import React, { FC } from 'react';
 
 interface Props {
@@ -12,7 +15,7 @@ const MessageCard: FC<Props> = ({message}) => {
     const {currentContact} = useCurrentContact();
 
     return (
-        <div className={cn('m-2.5 font-medium text-xs flex', 
+        <div className={cn('m-2.5 font-light text-xs flex', 
             message.receiver._id !== currentContact?._id 
             ? 'justify-start' 
             : 'justify-end'
@@ -24,12 +27,29 @@ const MessageCard: FC<Props> = ({message}) => {
             )}>
                 <p className={cn('text-sm', 
                     message.receiver._id !== currentContact?._id 
-                    ? 'text-white' 
-                    : ''
+                    ? 'text-zinc-700 dark:text-white' 
+                    : 'text-white'
                 )}>
                     {message.text}
                 </p>
-                <span className='absolute text-xs right-1 bottom-0 opacity-60'>✓</span>
+                <div className={cn('flex gap-0.75 absolute text-[9px] right-1 bottom-0 opacity-60',
+                    message.receiver._id !== currentContact?._id
+                    ? 'text-zinc-700 dark:text-white'
+                    : 'text-white'
+                )}>
+                    <p>{format(message.updatedAt, 'hh:mm a')}</p>
+                    <div className='self-end'>
+                        {
+                            message.receiver._id === currentContact?._id && (
+                                message.status === CONST.READ ? (
+                                    <CheckCheck size={12} />
+                                ) : (
+                                    <Check size={12} />
+                                )
+                            )
+                        }
+                    </div>
+                </div>
             </div>
         </div>
     );
