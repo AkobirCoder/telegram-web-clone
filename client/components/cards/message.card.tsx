0eea8c415b04405f493775ce502cd5549a53d602
AkobirCoder <akobir0308@gmail.com>
2026-03-30 01:37:59 +0500

    client/components/cards/message.card.tsx was updated

--- client/components/cards/message.card.tsx
+++ client/components/cards/message.card.tsx
@@ -6,6 +6,7 @@ import { format } from 'date-fns';
 import { Check, CheckCheck, Edit2, Trash } from 'lucide-react';
 import React, { FC } from 'react';
 import { ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuSeparator, ContextMenuTrigger } from '../ui/context-menu';
+import Image from 'next/image';
 
 interface Props {
     // isReceived?: boolean,
@@ -27,18 +28,27 @@ const MessageCard: FC<Props> = ({message, onReactionMessage, onDeleteMessage}) =
         )}>
             <ContextMenu>
                 <ContextMenuTrigger asChild>
-                    <div className={cn('relative inline p-2.5 pr-12 max-w-full', 
+                    <div className={cn('relative inline p-2.5 pr-12 max-w-full backdrop-blur-md', 
                         message.receiver._id === currentContact?._id 
                         ? 'bg-primary' 
                         : 'bg-secondary-foreground/20'
                     )}>
-                        <p className={cn('text-sm', 
-                            message.receiver._id !== currentContact?._id 
-                            ? 'text-zinc-700 dark:text-white' 
-                            : 'text-white'
-                        )}>
-                            {message.text}
-                        </p>
+                        {
+                            message.image && (
+                                <Image src={message.image} alt={message.image} width={300} height={250} />
+                            )
+                        }
+                        {
+                            message.text.length > 0 && (
+                                <p className={cn('text-sm', 
+                                    message.receiver._id !== currentContact?._id 
+                                    ? 'text-zinc-700 dark:text-white' 
+                                    : 'text-white'
+                                )}>
+                                    {message.text}
+                                </p>
+                            )
+                        }
                         <div className={cn('flex gap-0.75 absolute text-[9px] right-1 bottom-0 opacity-60',
                             message.receiver._id !== currentContact?._id
                             ? 'text-zinc-700 dark:text-white'
@@ -82,9 +92,13 @@ const MessageCard: FC<Props> = ({message, onReactionMessage, onDeleteMessage}) =
                         message.sender._id !== currentContact?._id && (
                             <>
                                 <ContextMenuSeparator />
-                                <ContextMenuItem className='cursor-pointer' onClick={() => setEditedMessage(message)}>
-                                    <Edit2 size={14} className='mr-2' /> <span>Edit</span>
-                                </ContextMenuItem>
+                                {
+                                    !message.image && (
+                                        <ContextMenuItem className='cursor-pointer' onClick={() => setEditedMessage(message)}>
+                                            <Edit2 size={14} className='mr-2' /> <span>Edit</span>
+                                        </ContextMenuItem>
+                                    )
+                                }
                                 <ContextMenuItem className='cursor-pointer' onClick={() => onDeleteMessage(message._id)}>
                                     <Trash size={14} className='mr-2' /> <span>Delete</span>
                                 </ContextMenuItem>
