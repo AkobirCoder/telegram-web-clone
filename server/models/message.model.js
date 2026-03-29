917679a7a3644f42426a56900a5a9605a70e0fd0
AkobirCoder <akobir0308@gmail.com>
2026-03-30 01:38:51 +0500

    server/models/message.model.js was updated

--- server/models/message.model.js
+++ server/models/message.model.js
@@ -6,7 +6,7 @@ const messageSchema = new Schema (
         sender: { type: Schema.ObjectId, ref: 'User', required: true },
         receiver: { type: Schema.ObjectId, ref: 'User', required: true },
         text: { type: String },
-        media: { type: String },
+        image: { type: String },
         status: { type: String, enum: [CONST.DELIVERED, CONST.READ, CONST.SENT], default: CONST.SENT },
         reaction: { type: String },
         editedStatus: {type: Boolean},
