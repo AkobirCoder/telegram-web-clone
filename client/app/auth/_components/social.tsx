c41c754cf2b538875eadc00fdc7ca050a0b862ce
AkobirCoder <akobir0308@gmail.com>
2026-03-31 01:36:47 +0500

    client/app/auth/_components/social.tsx was updated

--- client/app/auth/_components/social.tsx
+++ client/app/auth/_components/social.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
+'use client'
+
+import React, { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { FaGithub, FaGoogle } from 'react-icons/fa';
+import { signIn } from 'next-auth/react';
 
 const Social = () => {
+    const [isLoading, setLoading] = useState(false);
+
+    const onSignIn = async (provider: string) => {
+        setLoading(true);
+
+        await signIn(provider, {callbackUrl: '/'});
+    }
+
     return (
         <div className='grid grid-cols-2 w-full gap-1'>
-            <Button variant={'outline'}>
+            <Button variant={'outline'} onClick={() => onSignIn('google')} disabled={isLoading}>
                 <span>Sign up with Google</span>
                 <FaGoogle />
             </Button>
-            <Button variant={'secondary'}>
+            <Button variant={'secondary'} className='border-none' onClick={() => onSignIn('github')} disabled={isLoading}>
                 <span>Sign up with GitHub</span>
                 <FaGithub />
             </Button>
